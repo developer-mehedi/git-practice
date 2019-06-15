80c5d2d7fd5363d5b33564acf2b629038e3348ed
kamrulislamcmt <44641354+kamrulislamcmt@users.noreply.github.com>
2019-06-15 12:56:00 +0600

    Added a new method called display to hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("VS Code is Awesome....");
+
+        greetings("Mehedi Hasan");
+    }
+
+    public static void greetings(String name) {
+        System.out.println("Welcome " + name);
     }
 }
