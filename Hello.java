907653bfb0d7cc21549145010fbb547d3871da60
kamrulislamcmt <44641354+kamrulislamcmt@users.noreply.github.com>
2019-06-15 15:41:04 +0600

    Removed two method and add display method

--- Hello.java
+++ Hello.java
@@ -2,10 +2,11 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("VS Code is Awesome....");
 
-        greetings("Leaning Git");
+        display();
     }
 
-    public static void greetings(String name) {
-        System.out.println(" It's awesome " + name);
+    public static void display() {
+        System.out.println(" Hello Hello! ")
     }
+
 }
