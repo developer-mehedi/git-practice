5ad8a942d8aafd8d8ede6f74b2f4a7c8f2dec340
kamrulislamcmt <44641354+kamrulislamcmt@users.noreply.github.com>
2019-06-17 12:23:23 +0600

    Deleted previous sqr method and add new area method

--- Hello.java
+++ Hello.java
@@ -3,8 +3,8 @@ public class Hello {
         System.out.println("VS Code is Awesome....");
     }
 
-    public statc void sqr(int x) {
-        return x*x;
+    public statc void area(int x, int y, int z) {
+        return x*y*z;
     }
 
 } 
